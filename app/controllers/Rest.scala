df4de2ea602df3822a8ee8bfa123b19aa8987087
Eric Weise <eweise@gmail.com>
2012-02-13 21:45:21 -0800

    cleaning up uploadPhoto

--- app/controllers/Rest.scala
+++ app/controllers/Rest.scala
@@ -13,40 +13,43 @@ object Rest extends Controller {
 
   def tastings = Action {
     request =>
+      println("tastings")
       val token = request.queryString.get("token").get.headOption
-
       token match {
         case None => BadRequest("no token supplied")
         case _ =>
-          println("token = " + token)
           Ok(Tasting.listAsJson(1))
       }
   }
 
   def uploadphoto = Action {
     request =>
-      val token = request.queryString.get("token").get.headOption
-      token match {
-        case None => BadRequest("no token supplied")
-        case _ =>
-          println("before multipart")
-          val body = request.body.asFormUrlEncoded
+    //      println("token = " + request.queryString.get("token"))
+    //      val token = request.queryString.get("token").get.headOption
+    //      token match {
+    //        case None => BadRequest("no token supplied")
+    //        case _ =>
+    //          println("before multipart")
+      val body = request.body.asFormUrlEncoded
 
-          body.foreach {
-            m =>
+      body.foreach {
+        m =>
+          m.get("token").get.headOption match {
+            case s: Some[String] =>
               m.get("image").get.headOption match {
                 case s: Some[String] =>
                   println("getting bytes...")
                   val bytes = Base64.decodeBase64(s.get.getBytes)
                   println("finished getting bytes")
-
                   val f = new File("test.jpg")
                   val fos = new FileOutputStream(f)
                   fos.write(bytes)
                   fos.close()
                 case _ =>
-
               }
+            case _ => {
+              BadRequest("no token specified")
+            }
           }
       }
       Ok(Tasting.listAsJson(1))
